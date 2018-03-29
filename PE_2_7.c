b2df7a4e6acd32c0b0da34fcfb30d4aa9622d9b6
goshinh <jiller@126.com>
2018-03-29 13:21:07 +0800

    找回误删的文件：以PE_2_开头

---
+++ PE_2_7.c
@@ -0,0 +1,21 @@
+/* Programming Exercise 2-7 */
+#include<stdio.h>
+void smile(void);
+
+int main(void)
+{
+   smile(); 
+   smile(); 
+   smile(); 
+   printf("\n");
+   smile(); 
+   smile(); 
+   printf("\n");
+   smile(); 
+   printf("\n");
+    return 0;   
+}
+void smile(void)
+{
+    printf("smile!");
+}
